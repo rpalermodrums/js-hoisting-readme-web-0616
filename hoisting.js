979bba9b95d595f64fd8cf162976e0fe8fe87caf
Ryan Palermo <stix250@gmail.com>
2016-07-19 17:57:40 -0400

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,8 +3,9 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
+
 }
 
 
@@ -12,8 +13,8 @@ function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  function thisIsCrazy (){
+    console.log("hey!!!");
   }
 }
 
@@ -24,7 +25,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
